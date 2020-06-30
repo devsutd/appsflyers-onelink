607296642c5119727a7b628c1f7665a66dc8fa23
jcubillas <josecubillas08@gmail.com>
2020-06-30 20:37:10 -0300

    added variable html

--- public/mcapp/js/htmlemailsDashborad.js
+++ public/mcapp/js/htmlemailsDashborad.js
@@ -307,6 +307,8 @@ function listLinks(data) {
       array.sort((a, b) => ((new Date(a.Modified) < new Date(b.Modified)) ? 1 : ((new Date(b.Modified) < new Date(a.Modified)) ? -1 : 0)));
   }
 
+  var html;
+  
   for (let index = 0; index < array.length; index++) {
       const element = array[index];
       html += `<option id="link${index}" class="slds-listbox__item">${element.LinkName}</option>`;
