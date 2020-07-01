f203624aaa07682f9ba4057df3d86a0bc9cc22c6
jcubillas <josecubillas08@gmail.com>
2020-07-01 15:29:13 -0300

    update variable name

--- public/mcapp/js/htmlemailsDashborad.js
+++ public/mcapp/js/htmlemailsDashborad.js
@@ -349,9 +349,9 @@ function loadEmailinModal(emails){
 
     emailModalSlot += `<div id="emailslot${i}">`;
     emailModalSlot += '<header class="slds-modal__header" style="background-color: #f3f2f2; text-align:left">';
-    emailModalSlot += `<span><b>Email name: </b></span>${element.name}<br>`;
-    emailModalSlot += `<span><b>Subjectline: </b>${element.views.subjectline.content}</span><br>`;
-    emailModalSlot += `<span><b>Preheader: </b>${element.views.preheader.content}</span>`;
+    emailModalSlot += `<span><b>Email name: </b></span>${emailHTML.name}<br>`;
+    emailModalSlot += `<span><b>Subjectline: </b>${emailHTML.views.subjectline.content}</span><br>`;
+    emailModalSlot += `<span><b>Preheader: </b>${emailHTML.views.preheader.content}</span>`;
     emailModalSlot += '</header>';
     emailModalSlot += `<div id="emailcontent${i}">`;
     emailModalSlot += '<label class="slds-form-element__label" for="select-01" style="padding-left: 1rem;padding-top: 1rem;">Select Links from the Email</label>';
